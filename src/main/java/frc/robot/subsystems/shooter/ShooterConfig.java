73d9751c6057c972c09502399d9db5aea87bd710
Zachary Taylor <zacharytaylor0508@gmail.com>
2026-02-24 08:03:46 -0600

    tuning 2/23/26

--- src/main/java/frc/robot/subsystems/shooter/ShooterConfig.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConfig.java
@@ -9,25 +9,28 @@ import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 public class ShooterConfig {
-    public static TalonFXConfiguration leftShooterConfiguration() {
-        return new TalonFXConfiguration().withCurrentLimits(currentLimits).withSlot0(slot0Configs)
-              //  .withMotionMagic(motionMagic)
-                .withMotorOutput(new MotorOutputConfigs().withInverted(InvertedValue.CounterClockwise_Positive)
-                        .withNeutralMode(NeutralModeValue.Coast));
-    }
+        public static TalonFXConfiguration leftShooterConfiguration() {
+                return new TalonFXConfiguration().withCurrentLimits(currentLimits).withSlot0(slot0Configs)
+                                .withMotionMagic(motionMagic)
+                                .withMotorOutput(new MotorOutputConfigs()
+                                                .withInverted(InvertedValue.CounterClockwise_Positive)
+                                                .withNeutralMode(NeutralModeValue.Coast));
+        }
 
-    public static TalonFXConfiguration rightShooterConfiguration() {
-        return new TalonFXConfiguration().withCurrentLimits(currentLimits).withSlot0(slot0Configs)
-             //   .withMotionMagic(motionMagic)
-                .withMotorOutput(new MotorOutputConfigs().withInverted(InvertedValue.Clockwise_Positive)
-                        .withNeutralMode(NeutralModeValue.Coast));
-    }
+        public static TalonFXConfiguration rightShooterConfiguration() {
+                return new TalonFXConfiguration().withCurrentLimits(currentLimits).withSlot0(slot0Configs)
+                                .withMotionMagic(motionMagic)
+                                .withMotorOutput(new MotorOutputConfigs().withInverted(InvertedValue.Clockwise_Positive)
+                                                .withNeutralMode(NeutralModeValue.Coast));
+        }
 
-    static CurrentLimitsConfigs currentLimits = new CurrentLimitsConfigs().withStatorCurrentLimit(60)
-            .withStatorCurrentLimitEnable(true).withSupplyCurrentLimit(40).withSupplyCurrentLimitEnable(true);
-            // KI (7.5)  
-    static Slot0Configs slot0Configs = new Slot0Configs().withKP(0.5).withKI(7.5).withKD(0).withKS(0.14).withKV(0.11167).withKA(0);
-    static MotionMagicConfigs motionMagic = new MotionMagicConfigs().withMotionMagicAcceleration(9999)
-            .withMotionMagicCruiseVelocity(0).withMotionMagicJerk(0);
+        static CurrentLimitsConfigs currentLimits = new CurrentLimitsConfigs().withStatorCurrentLimit(100)
+                        .withStatorCurrentLimitEnable(true).withSupplyCurrentLimit(50)
+                        .withSupplyCurrentLimitEnable(true);
+        // KI (7.5) 1
+        static Slot0Configs slot0Configs = new Slot0Configs().withKP(0.5).withKI(0).withKD(0).withKS(0.14)
+                        .withKV(0.11167).withKA(0);
+        static MotionMagicConfigs motionMagic = new MotionMagicConfigs().withMotionMagicAcceleration(9999)
+                        .withMotionMagicCruiseVelocity(0).withMotionMagicJerk(0);
 
 }
