d115a9e0f02eaf22ddf5202af2924c25d173e184
Zachary Taylor <zacharytaylor0508@gmail.com>
2026-02-27 23:52:01 -0600

    .

--- src/main/java/frc/robot/subsystems/vision/VisionLimelightIO.java
+++ src/main/java/frc/robot/subsystems/vision/VisionLimelightIO.java
@@ -1,21 +1,20 @@
 package frc.robot.subsystems.vision;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import limelight.Limelight;
-import limelight.networktables.LimelightResults;
 
 public class VisionLimelightIO implements VisionIO {
 
+    public double TX;
+    public double TY;
+    public double TA;
     public boolean TV;
     public String pipeLine;
     public Pose2d foundPosition;
-    // private LimelightHelpers.PoseEstimate limelightMeasurement;
-    private Limelight limeLight;
+    public double timeStamp;
+    private LimelightHelpers.PoseEstimate limelightMeasurement;
 
     public VisionLimelightIO(String pipeLine) {
         this.pipeLine = pipeLine;
-
-        this.limeLight = new Limelight(pipeLine);
     }
 
     @Override
@@ -24,7 +23,14 @@ public class VisionLimelightIO implements VisionIO {
     }
 
     @Override
-    public void setVisionNeutral() {}
+    public void setVisionNeutral() {
+        TX = 0;
+        TY = 0;
+        TA = 0;
+        TV = false;
+        foundPosition = new Pose2d();
+        timeStamp = 0;
+    }
 
 @Override
 public void updateTracking(double yawDegrees) {
@@ -40,6 +46,10 @@ public void updateTracking(double yawDegrees) {
 
     @Override
     public void trackingStart() {
+        TX = LimelightHelpers.getTX(pipeLine);
+        TY = LimelightHelpers.getTY(pipeLine);
+        TA = LimelightHelpers.getTA(pipeLine);
+        TV = LimelightHelpers.getTV(pipeLine);
 
     }
 
